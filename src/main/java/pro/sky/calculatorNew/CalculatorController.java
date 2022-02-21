4f2c8dce62f1a0ce15fbb4a8bba8b9e78a9ab00d
NikitaShkril <Shkril95@yandex.ru>
2022-02-21 12:26:07 +0300

    Working!

--- src/main/java/pro/sky/calculatorNew/CalculatorController.java
+++ src/main/java/pro/sky/calculatorNew/CalculatorController.java
@@ -1,32 +1,38 @@
 package pro.sky.calculatorNew;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-
+@RequestMapping("/calculator")
 @RestController
 public class CalculatorController {
+    private final CalculatorSerivce calculatorSerivce;
 
-    @GetMapping("/calculator")
+    public CalculatorController(CalculatorSerivce calculatorSerivce) {
+        this.calculatorSerivce = calculatorSerivce;
+    }
+
+    @GetMapping
     public String greeting() {
         return "Добро пожаловать в калькулятор!";
     }
 
     @GetMapping("/plus")
     public String plus(@RequestParam("num1") int num1, @RequestParam("num2") int num2) {
-        return num1 + " + " + num2 + " = " + plus(num1, num2);
+        return num1 + " + " + num2 + " = " + calculatorSerivce.plus(num1, num2);
     }
     @GetMapping("/minus")
     public String minus(@RequestParam("num1") int num1, @RequestParam("num2") int num2) {
-        return num1 + " - " + num2 + " = " + minus(num1, num2);
+        return num1 + " - " + num2 + " = " + calculatorSerivce.minus(num1, num2);
     }
     @GetMapping("/multiply")
     public String multiply(@RequestParam("num1") int num1, @RequestParam("num2") int num2) {
-        return num1 + " * " + num2 + " = " + multiply(num1, num2);
+        return num1 + " * " + num2 + " = " + calculatorSerivce.multiply(num1, num2);
     }
     @GetMapping("/divide")
     public String divide(@RequestParam("num1") int num1, @RequestParam("num2") int num2) {
-        return num1 + " / " + num2 + " = " + divide(num1, num2);
+        return num1 + " / " + num2 + " = " + calculatorSerivce.divide(num1, num2);
     }
 }
